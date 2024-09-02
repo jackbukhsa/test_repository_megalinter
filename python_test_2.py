477ddcdde1cedec33e222e2af452daf9301e2ea2
jackbukhsa <jack.biggs@ukhsa.gov.uk>
2024-09-02 19:09:02 +0100

    change to adhere to black

--- python_test_2.py
+++ python_test_2.py
@@ -2,15 +2,15 @@
 This module contains an example function to demonstrate
 the usage of docstrings and Pylint error handling.
 """
+
 import os
 
 print(os.getcwd())
 
 
 def example_function(param1, param2):
-
     """
-    This is an example function that will compare two prints and see if they are equal 
+    This is an example function that will compare two prints and see if they are equal
     """
     # This function has several issues
     if param1 == param2:
@@ -23,7 +23,7 @@ def example_function(param1, param2):
 
 def main():
     """
-    This is the main function that will call the example function 
+    This is the main function that will call the example function
     """
 
     example_function("hello", "world")
